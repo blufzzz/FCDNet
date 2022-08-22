54af73c0caef6fbaf33361002661730109eb1a9e
relyativist <yarkin.vyacheslav@mail.ru>
2022-08-22 13:48:04 +0300

    Visualization are now in metrics, created new fancy plots

--- metrics.py
+++ metrics.py
@@ -22,15 +22,7 @@ def calculate_metrics(data_resica, data_active):
         large_val = 0
         thresh_val = 0
         tn, fp, fn, tp = confusion_matrix((data_active > 0.5).reshape(-1), (data_resica > thresh).reshape(-1)).ravel()
-        #print(f'Threshold {thresh}', f'Size of predicted_label {(np.where((data_resica > thresh),1,0)).sum()}')
         large_enough = int((np.where((data_resica > thresh),1,0)).sum() > 800)
-        # if large_enough > 0:
-        #     large_val = (np.where((data_resica > thresh),1,0)).sum()
-        #     thresh_val = thresh
-        #     print(f'============Final thresh {thresh_val}=============')
-        #     print(f'============Final val {large_val}=============')
-        #metrics.append([thresh, large_enough*tp/(tp+fp),large_enough*tp/(tp+fn),large_enough*tn/(tn+fp),
-        #                large_enough*(tp+tn)/(tp+tn+fp+fn),large_enough*fp/(fn+fp),large_enough*2*tp/(2*tp+fp+fn),large_enough*tp/(tp+fp+fn)])
         metrics.append([thresh, large_enough*tp/(tp+fp),large_enough*tp/(tp+fn),large_enough*tn/(tn+fp),large_enough*2*tp/(2*tp+fp+fn)])
         
     metrics = np.nan_to_num(np.array(metrics))
